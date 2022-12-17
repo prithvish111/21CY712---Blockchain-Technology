c7577c697d94793af42ce54fae9171ea0c202a3c
Prithvish N A <prithvish111@gmail.com>
2022-12-17 14:45:09 +0530

    Update myContract.sol

--- miniProject/Smart Contract/myContract.sol
+++ miniProject/Smart Contract/myContract.sol
@@ -2,6 +2,7 @@
 
 pragma solidity >=0.7.0 <0.9.0;
 
+//import "hardhat/console.sol";
 
 /**
  * @title myContract
@@ -12,11 +13,19 @@ contract metaverse{
     mapping(string => user) userList;
 
     struct user{
+        land l;
         string name;
         string emailAddress;
         address walletAddress;
         string regNo;
         bool exists;
+    }
+
+    struct land{
+        string regNo;
+        string landName;
+        string nftName;
+        uint256 price;
     }
 
     function register(string memory _name, string memory _emailAddress, address _walletAddress, string memory _regNo) public {
@@ -33,9 +42,27 @@ contract metaverse{
     function alreadyMember(string memory _regNo) public view returns(bool){
         user memory u;
         u.regNo = _regNo;
+
         if(userList[u.regNo].exists){
             return true;
         }
         return false;
+    }
+
+    function addLandInfo(string memory _regNo, string memory _landName, string memory _nftName, uint256 _price) public{
+        land memory l;
+        l.regNo = _regNo;
+        l.landName = _landName;
+        l.nftName = _nftName;
+        l.price = _price;
+
+        userList[l.regNo].l = l;
+    }
+
+    function fetchLandInfo(string memory _regNo) public view returns(string memory, string memory, uint256){
+        land memory l;
+
+        l = userList[_regNo].l;
+        return(l.landName, l.nftName, l.price);
     }
 }
