6d587c16e466de8d0b6546bac147af783dda8142
Prithvish N A <prithvish111@gmail.com>
2022-12-17 14:49:51 +0530

    Add files via upload

--- miniProject/init.js
+++ miniProject/init.js
@@ -3,6 +3,82 @@ var abi = [
 		"inputs": [
 			{
 				"internalType": "string",
+				"name": "_regNo",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "_landName",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "_nftName",
+				"type": "string"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_price",
+				"type": "uint256"
+			}
+		],
+		"name": "addLandInfo",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "_regNo",
+				"type": "string"
+			}
+		],
+		"name": "alreadyMember",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "_regNo",
+				"type": "string"
+			}
+		],
+		"name": "fetchLandInfo",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			},
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
 				"name": "_name",
 				"type": "string"
 			},
@@ -26,26 +102,7 @@ var abi = [
 		"outputs": [],
 		"stateMutability": "nonpayable",
 		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "string",
-				"name": "_regNo",
-				"type": "string"
-			}
-		],
-		"name": "alreadyMember",
-		"outputs": [
-			{
-				"internalType": "bool",
-				"name": "",
-				"type": "bool"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
 	}
 ];
 
-var contractaddress = '0x406AB5033423Dcb6391Ac9eEEad73294FA82Cfbc';
+var contractaddress = '0xfDe48DfCdC603B2BE058FE786e6f5cA8699ba87b';
